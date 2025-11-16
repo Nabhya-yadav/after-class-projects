c54651c6797b61d0d6acfe5c028a97f778259829
Nabhya-yadav <nabhya.yadav20@gmail.com>
2025-11-16 19:04:33 +0530

    Add files via upload

---
+++ agecounter.py
@@ -0,0 +1,18 @@
+age=int(input("Enter your age"))
+
+if age==str:
+    raise ValueError
+
+else:
+    a=age/2
+    if age==0:
+        print("Even number")
+    else:
+        print("Odd number")
+
+
+    
+    
+
+
+
